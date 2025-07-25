b17c22f651a61a67d719a205458da7b65ad74ac1
adriandermawan <adriandermawan123@gmail.com>
2025-07-25 23:17:47 +0700

    first init github

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+const scrollUp = document.querySelector(".scroll-up");
+
+window.addEventListener("scroll", () => {
+    
+    if(window.scrollY > 500) {
+        scrollUp.classList.add("scroll-active");
+    } else {
+        scrollUp.classList.remove("scroll-active");
+    }
+    console.log(window.scrollY);
+});
